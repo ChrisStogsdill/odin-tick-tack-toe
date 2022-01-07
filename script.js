946e49817e6a6a0eb081d80bc6554564698f63d8
ChrisStogsdill <chris1stogsdill@gmail.com>
2022-01-07 16:25:27 -0600

    fille out HTML and start on script

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const player1Button = document.getElementById('player1-button')
+const player1Form = document.getElementById('player1-form')
+
+player1Button.addEventListener('click', (clickEvent) => {
+    player1Form.style.display = 'flex';
+})
+
